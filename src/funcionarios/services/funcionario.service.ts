5c951c253ce08037e898fa964fbfbaac48e965bb
Beatriz Braga <beatrizsagrado9@gmail.com>
2026-04-07 12:15:21 -0300

    feat: funcionario.service

--- src/funcionarios/services/funcionario.service.ts
+++ src/funcionarios/services/funcionario.service.ts
@@ -10,4 +10,9 @@ export class FuncionarioService{
         @InjectRepository(Funcionario) 
         private funcionarioRepository: Repository<Funcionario>  
     ){}
+
+    //funçao que retorna todos funcionarios e suas informações
+    async findAll(): Promise <Funcionario[]> {
+        return await this.funcionarioRepository.find();
+    }
 }
