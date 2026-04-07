7961f75d236bf5375ab54ac78c54a6d812918459
lucaaas-araujo <lucasaraujofernandes.dev@hotmail.com>
2026-04-07 14:40:35 -0300

    feat: adicionar metodos crud

--- src/funcionarios/controllers/funcionario.controller.ts
+++ src/funcionarios/controllers/funcionario.controller.ts
@@ -1,7 +1,8 @@
 //importações
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { Funcionario } from "../entities/funcionario.entity";
 import { FuncionarioService } from "../services/funcionario.service";
+import { DeleteResult } from "typeorm";
 
 // controlador para a entidade Funcionario funcionar
 @Controller("/funcionario")
@@ -26,6 +27,25 @@ export class FuncionarioController {
     @HttpCode(HttpStatus.OK)
     findByAllDepartamento(@Param('departamento')departamento: string): Promise<Funcionario[]>{
         return this.funcionarioService.findAllByDepartamento(departamento);
-        
     }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() funcionario: Funcionario): Promise<Funcionario> {
+        return this.funcionarioService.create(funcionario);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() funcionario: Funcionario): Promise<Funcionario> {
+        return this.funcionarioService.update(funcionario);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+        return this.funcionarioService.delete(id);
+    }
+
+
 }
